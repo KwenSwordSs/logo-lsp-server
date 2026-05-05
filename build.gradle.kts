08e718a00ad5cf6837c7bcd3f081a1f66b14ea7e
melisaoner <upjcm@student.kit.edu>
2026-05-05 16:46:23 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    java
+    application
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.eclipse.lsp4j:org.eclipse.lsp4j:0.21.1")
+    implementation("org.eclipse.lsp4j:org.eclipse.lsp4j.jsonrpc:0.21.1")
+}
+
+application {
+    mainClass.set("Main")
+}
