265ab8a7fadf908e957431cbbb87682973c9038a
melisaoner <upjcm@student.kit.edu>
2026-05-07 14:44:44 +0200

    gradle.kts correction

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,15 +16,14 @@ repositories {
 dependencies {
     implementation("org.eclipse.lsp4j:org.eclipse.lsp4j:0.21.1")
     implementation("org.eclipse.lsp4j:org.eclipse.lsp4j.jsonrpc:0.21.1")
+
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
 }
 
 application {
     mainClass.set("logo.lsp.Main")
 }
 
-dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
-}
 
 tasks.test {
     useJUnitPlatform()
